0b82de02f45745dcda8b1139ced63748572ee3fb
Nils Sowen <n.sowen@2scale.net>
2021-01-15 16:25:47 +0100

    ISSUE-8: Shutter Contact support

--- src/devices/HmIPPushButton.ts
+++ src/devices/HmIPPushButton.ts
@@ -8,7 +8,7 @@ import {HmIPGenericDevice} from './HmIPGenericDevice';
  * HomematicIP Push Button
  */
 export class HmIPPushButton extends HmIPGenericDevice implements Updateable {
-  private service: Service;
+  //private service: Service;
 
   constructor(
     platform: HmIPPlatform,
@@ -18,9 +18,13 @@ export class HmIPPushButton extends HmIPGenericDevice implements Updateable {
     super(platform, home, accessory);
 
     this.platform.log.debug(`Created HmIPPushButton ${accessory.context.device.label}`);
+    /*
+    FIXME: how to read state of button? SINGLE_KEY_CHANNEL index=1 "upper", and the other is "lower" button, but the state
+           is not sent on the API.
+
     this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.label);
-
+    */
     this.updateDevice(home, accessory.context.device, platform.groups);
   }
 
