8094f63a0ded9cd44d54b2f4ff0e114bd02d910c
Dmitry Malchikov <dmalch@gmail.com>
2021-11-25 21:05:32 +0100

    Minor fixes

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,12 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomematicIP-dmalch';
+export const PLATFORM_NAME = 'HomematicIP';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-homematicip-dmalch';
+export const PLUGIN_NAME = 'homebridge-homematicip';
 
 /**
  * Version to be used in protocol communication
