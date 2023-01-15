a80feb980cd45eac59c5f84c5b925a931af7cfdf
Nils Sowen <n.sowen@2scale.net>
2023-01-15 12:30:01 +0100

    unified HmIPHeatingThermostat, HmIPWallMountedThermostat; fixed missing fakegato statistics; limited fakegato updates

---
+++ src/devices/HmIPWallMountedHeatingThermostat.ts
@@ -0,0 +1,96 @@
+import {
+  CharacteristicGetCallback,
+  PlatformAccessory,
+} from 'homebridge';
+
+import {HmIPPlatform} from '../HmIPPlatform';
+import {HmIPDevice, HmIPFunctionalChannel, HmIPGroup, HmIPHeatingGroup, Updateable} from '../HmIPState';
+import {HistoryEvent, HmIPHeatingThermostat, ThermostatChannel} from './HmIPHeatingThermostat';
+import moment from 'moment/moment';
+
+interface WallMountedThermostatChannel extends ThermostatChannel {
+  actualTemperature: number;
+  humidity: number;
+}
+
+interface WallMountedThermostatInternalSwitchChannel {
+  functionalChannelType: string;
+  valvePosition: number;
+}
+
+/**
+ * HomematicIP Wall Mounted Heating Thermostat and climate sensors
+ *
+ * HmIP-WTH
+ * HmIP-WTH-2
+ * HMIP-WTH-B
+ * HmIP-BWTH
+ * HmIP-STH
+ * HmIP-STHD
+ * ALPHA-IP-RBG
+ */
+export class HmIPWallMountedHeatingThermostat extends HmIPHeatingThermostat implements Updateable {
+  private humidity = 0;
+
+  constructor(
+    platform: HmIPPlatform,
+    accessory: PlatformAccessory,
+  ) {
+    super(platform, accessory);
+
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity)
+      .on('get', this.handleCurrentRelativeHumidityGet.bind(this));
+  }
+
+  updateDevice(hmIPDevice: HmIPDevice, groups: { [p: string]: HmIPGroup }) {
+    for (const id in hmIPDevice.functionalChannels) {
+      const channel = hmIPDevice.functionalChannels[id];
+      if (channel.functionalChannelType === 'INTERNAL_SWITCH_CHANNEL') {
+        // this.platform.log.debug('internalSwitchChannel', JSON.stringify(channel));
+        const wthsChannel = <WallMountedThermostatInternalSwitchChannel>channel;
+        this.updateValvePosition(wthsChannel.valvePosition, 'internalSwitchChannel');
+      }
+      if (channel.functionalChannelType === 'WALL_MOUNTED_THERMOSTAT_PRO_CHANNEL'
+        || channel.functionalChannelType === 'WALL_MOUNTED_THERMOSTAT_WITHOUT_DISPLAY_CHANNEL') {
+        const wthChannel = <WallMountedThermostatChannel>channel;
+        this.updateSetPointTemperature(wthChannel.setPointTemperature, 'device channel');
+        this.updateActualTemperature(wthChannel.actualTemperature);
+        this.updateHumidity(wthChannel.humidity);
+      }
+    }
+    // call super method that manages heating groups etc.
+    super.updateDevice(hmIPDevice, groups);
+  }
+
+  protected updateHumidity(updatedHumidity: number) {
+    if (updatedHumidity !== null && updatedHumidity !== this.humidity) {
+      this.humidity = updatedHumidity;
+      this.platform.log.debug('Current relative humidity of %s changed to %s %%', this.accessory.displayName, this.humidity);
+      this.service.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, this.humidity);
+    }
+  }
+
+  protected updateByHeatingGroup(heatingGroup: HmIPHeatingGroup, channel: HmIPFunctionalChannel) {
+    super.updateByHeatingGroup(heatingGroup, channel);
+    this.updateValvePosition(heatingGroup.valvePosition, 'group'); // if heatingGroup provides valvePosition: use it
+  }
+
+  protected createHistoryEvent(): HistoryEvent {
+    return {
+      time: moment().unix(),
+      setTemp: this.setPointTemperature,
+      valvePosition: this.getCurrentValvePositionAsInt(),
+      humidity: this.humidity,
+      temp: this.actualTemperature,
+    };
+  }
+
+  protected getHistoryEventType(): string {
+    return 'custom';
+  }
+
+  handleCurrentRelativeHumidityGet(callback: CharacteristicGetCallback) {
+    callback(null, this.humidity);
+  }
+
+}
