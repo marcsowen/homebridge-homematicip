9697f94c3405b86de1ce864b1e1cd1942c74dbf6
Marc Sowen <marc@sowen.net>
2026-08-03 11:31:38 +0200

    Fixed version number handling

--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,20 @@
+import {createRequire} from 'node:module';
+
+interface PackageMetadata {
+  name: string;
+  version: string;
+}
+
+const require = createRequire(import.meta.url);
+const packageMetadata = require('../package.json') as PackageMetadata;
+
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
 export const PLATFORM_NAME = 'HomematicIP';
 
 /**
- * This must match the name of your plugin as defined the package.json
+ * Package metadata used for Homebridge registration and protocol communication
  */
-export const PLUGIN_NAME = 'homebridge-homematicip';
-
-/**
- * Version to be used in protocol communication
- */
-export const PLUGIN_VERSION = '2.0.1';
+export const PLUGIN_NAME = packageMetadata.name;
+export const PLUGIN_VERSION = packageMetadata.version;
